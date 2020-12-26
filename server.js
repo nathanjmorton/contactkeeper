efdde957f93eeec0bcfe433df1b14ca683df208d
Nathan Morton <nathan.j.morton@gmail.com>
2020-12-26 13:15:20 -0500

    this

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
-const app = express();
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Contact Keeper API' })
