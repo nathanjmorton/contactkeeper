e1f6cb81ae4dca3a9c50ee6bbcf501319b8f9e62
Nathan Morton <nathan.j.morton@gmail.com>
2020-12-27 20:13:49 -0500

    this

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Alerts from './components/layout/Alerts';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Login from './components/auth/Login';
+import PrivateRoute from './components/routing/PrivateRoute';
 import Register from './components/auth/Register';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -27,7 +28,7 @@ const App = () => {
               <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
